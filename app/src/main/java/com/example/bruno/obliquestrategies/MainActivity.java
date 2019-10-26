efcd56c3a48d418921c4f3acdd42bac31393fcea
RiffLord <bruno.pezer@hotmail.com>
2019-10-26 18:24:18 +0200

    Preliminary code cleanup & comments added. Refinement to the card-drawing algorithm needed.

--- app/src/main/java/com/example/bruno/obliquestrategies/MainActivity.java
+++ app/src/main/java/com/example/bruno/obliquestrategies/MainActivity.java
@@ -16,11 +16,11 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Random;
 
-//  TODO: CODE CLEANUP
-
 public class MainActivity extends AppCompatActivity {
+    //  Tag for log messages
     private static final String TAG = "Eno";
 
+    //  View of the screen
     private View mScreenView;
 
     /** TODO:
@@ -38,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
 
         Log.i(TAG, "onCreate... Instantiating layout.");
 
-        //  Instantiates the layout and obtains a reference to it in order to add a listener
+        //  Obtains a reference to the activity_main.xml layout
         mScreenView = findViewById(R.id.layout);
     }
 
@@ -46,12 +46,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         Log.i(TAG, "onStart... Touch listener set up.");
+
+        //  Sets up a listener which waits for a touch event
         mScreenView.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                //  Handles the touch by calling the drawCard method
                 Log.i(TAG, "Received a Touch event...");
 
+                //  Handles the touch by calling the drawCard method
                 drawCard(v);
 
                 return false;
@@ -71,22 +73,36 @@ public class MainActivity extends AppCompatActivity {
         Log.i(TAG, "onRestart... Calling onStart.");
     }
 
+    //  Accesses the deck file and draws a random card from it
     private void drawCard(View view) {
         Log.i(TAG, "Drawing a new card...");
 
+        //  Obtains a reference to the subtitle TextView and hides it from the screen
+        final TextView subtitle = findViewById(R.id.subtitle);
+        subtitle.setVisibility(View.INVISIBLE);
+
+        //  Obtains a reference to the title TextView of the layout;
+        //  the TextView will be used to display the card
+        final TextView card = findViewById(R.id.title);
+
         //  TODO: keep an array of drawn card indexes
+
+        //  Obtains a reference to the assets directory
         AssetManager assets = view.getContext().getAssets();
 
-            //  Counts the cards in the deck file
         int nCards = 0;
         try {
             Log.i(TAG, "Opening file to count cards...");
+
+            //  Opens the deck file & BufferedReader...
             InputStream deckStream = assets.open("deck");
             BufferedReader deckReader = new BufferedReader(new InputStreamReader(deckStream));
 
+            //  Counts the cards in the deck file
             while (deckReader.readLine() != null) {
                 nCards++;
             }
+            //  ...BufferedReader is closed
             deckReader.close();
 
             Log.i(TAG, "Number of cards in deck: " + nCards);
@@ -95,18 +111,26 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
-        //  Iterates through the file up to the desired index and and prints the corresponding card
+        //  Creates a String instance to which the drawn card's text will be assigned
         String cardContent = new String();
         try {
             Log.i(TAG, "Opening file to draw a random card...");
+
+            //  Reopens the file & reader...
             InputStream deckStream = assets.open("deck");
             BufferedReader deckReader = new BufferedReader(new InputStreamReader(deckStream));
 
+            //  Used for pseudo-random number generation
             Random cardToDraw = new Random();
             cardToDraw.setSeed(SystemClock.currentThreadTimeMillis());
 
+            //  Generates a random number, no bigger than the value of nCards
+            //  and iterates through the file up to that number
             for (int i = 0; i < cardToDraw.nextInt(nCards); i++)
                 cardContent = deckReader.readLine();
+
+            //  ...reader is finally closed
+            deckReader.close();
 
             Log.i(TAG, cardContent);
         } catch (IOException e) {
@@ -114,14 +138,14 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
-        final TextView subtitle = findViewById(R.id.subtitle);
-        subtitle.setVisibility(View.INVISIBLE);
-
-        final TextView card = findViewById(R.id.title);
-
+        //  Sets the text to the value obtained from the deck
         if (cardContent != null)
             card.setText(cardContent);
 
-        else card.setText("New card drawn!");
+        //  If the [blank] card was drawn, hides the TextView
+        else if (cardContent.equals("[blank]"))
+            card.setVisibility(View.INVISIBLE);
+
+        else card.setText(R.string.card_missing);
     }
 }
